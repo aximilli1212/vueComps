c071107b8053f9d0dffd08a04420c6b448a922f9
Emmanuel Sackey <aximilli1212@gmail.com>
2023-07-25 19:54:38 +0000

    Add props for Alert

--- composition-nomix/src/App.vue
+++ composition-nomix/src/App.vue
@@ -1,28 +1,36 @@
 <template>
   <div>
     <p>{{num}}</p>
+    <p>Computed Double: {{double}}</p>
+
     <button @click.prevent="increment()" style="margin-bottom: 30px">Click ++</button> <br />
 
     <input type="text" v-model="phrase" /> <br />
     <p>Phrase: {{phrase}}</p> <br />
-    <p>Reversed Phrase: {{reversedPhrase}}</p>
+    <p>Reversed Phrase: {{reversedPhrase}}</p> <br />
+<!--    <app-alert :user="user"/>-->
   </div>
 </template>
-
 <script>
 import {useVal} from "./useVal";
+import AppAlert from "./components/Alert"
 
 export default {
   name: "App",
+  // components: {
+  //   AppAlert
+  // },
   setup (){
    const {
-     state:{num, phrase, reversedPhrase}, actions:{increment}} = useVal()
+     state:{num, phrase, reversedPhrase, double, user}, actions:{increment}} = useVal()
 
     return {
      num,
       increment,
       phrase,
-      reversedPhrase
+      reversedPhrase,
+      double,
+      user
     }
   }
 };
---
+++ composition-nomix/src/components/Alert.vue
@@ -0,0 +1,28 @@
+<template>
+ <div v-if="!flag">Name has changed to: <b>{{userName}}</b></div>
+</template>
+
+<script>
+import {computed} from "vue";
+
+export default {
+  name: "AppAlert",
+  props: ['user'],
+  setup(props){
+    const flag = computed(()=>{
+      return props.user.name === "Alexoo"
+    })
+    const userName = computed(()=>{
+      return props.user.name
+    })
+    return {
+      flag,
+      userName
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
